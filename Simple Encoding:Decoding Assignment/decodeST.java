56922bc5bee4c798b6968dc1165f8cb9de57e506
obedxlastick <91111500+obedxlastick@users.noreply.github.com>
2024-10-22 16:18:20 -0500

    Update decodeST.java

--- Simple Encoding:Decoding Assignment/decodeST.java
+++ Simple Encoding:Decoding Assignment/decodeST.java
@@ -1,96 +1,95 @@
 import java.io.*;
 import java.util.Scanner;
-import java.awt.image.BufferedImage; 
-import javax.imageio.ImageIO; 
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
-public class decodeST 
-{
-    public static String ENDING_MESSAGE = "&*09^%$__"; //Marks the end of the message
+public class decodeST {
+    public static String ENDING_MESSAGE = "&*09^%$__"; // super arbitrary. Just marks the end of the message
+                                                       // could and should probably also use a header at the beginnign
+                                                       // of the message to indicate size, but this is good enough for
+                                                       // now
 
-    
-    public static void main(String[] args)
-    {
-        //attempt to get image to decode
-    	try 
-        {
-    	    //gets the name of the image file to decode
+    public static void main(String[] args) {
+        // attempt to get image to decode
+        try {
+            // gets the name of the image file to decode
             System.out.println("Enter the name of the image you want to decode: ");
             Scanner userInput = new Scanner(System.in);
             String fileName = userInput.nextLine();
             userInput.close();
-            
-            //uses image path as FileInputStream argument
-    	    InputStream pngFile = new FileInputStream(fileName);
-	    BufferedImage pngImage = ImageIO.read(pngFile);
-			
-	    //decode message
+
+            // uses image path as FileInputStream argument
+            InputStream pngFile = new FileInputStream(fileName);
+            BufferedImage pngImage = ImageIO.read(pngFile);
+
+            // decode message
             System.out.println("Now trying to decode...");
 
-            //gets the contents of the message from beginning at the start of the file until the ENDING_MESSAGE delimiter
-            System.out.println("Decoded message = " + decodeMessage(pngImage).substring(0, decodeMessage(pngImage).indexOf(ENDING_MESSAGE)));
-		}
-    	
-        //image not found, inform user
-	catch(IOException e) 
-        {
-		System.out.println("Image not found");
-	}
+            // gets the contents of the message from beginning at the start of the file
+            // until the ENDING_MESSAGE delimiter
+            System.out.println("Decoded message = "
+                    + decodeMessage(pngImage).substring(0, decodeMessage(pngImage).indexOf(ENDING_MESSAGE)));
+        }
+
+        // image not found, inform user
+        catch (IOException e) {
+            System.out.println("Image not found");
+        }
     }
 
-    //Use masking to get final bit of argument integer
-    public static int decodeBit(int number)
-    {
-        return number & 1; //bitwise operator that returns the last bit of the number
-                           //if 0, returns 0. if 1, returns 1
+    // Use masking to get final bit of argument integer
+    public static int decodeBit(int number) {
+        return number & 1; // bitwise operator that returns the last bit of the number
+                           // if 0, returns 0. if 1, returns 1
     }
 
-    public static String decodeMessage(BufferedImage image)
-    {
+    public static String decodeMessage(BufferedImage image) {
         int pixelValue = 0;
         int height = image.getHeight();
         int width = image.getWidth();
-        int [] message = new int[height * width]; //each pixel is 1 bit, so total bits is height * width
+        int[] message = new int[height * width * 3]; // each pixel is 1 bit, so total bits is height * width. * 3 b/c 3
+                                                     // bits/pix
         int count = 0;
 
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                if (count < message.length) //messageLength is the length of the message in bits, found in encodeMessage method
-                { 
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (count < message.length)// messageLength is the length of the message in bits, found in encodeMessage
+                                           // method
+                {
                     pixelValue = image.getRGB(x, y);
-                    message[count++] = decodeBit(pixelValue);
+
+                    message[count++] = decodeBit((pixelValue >> 16) & 0xFF); // Get the LSB of red
+                    message[count++] = decodeBit((pixelValue >> 8) & 0xFF); // Get the LSB of green
+                    message[count++] = decodeBit(pixelValue & 0xFF); // Get the LSB of blue
+
                 }
             }
         }
-        
+
         return bitsToMessage(message);
     }
 
     // Converts bits of message into ASCII text
     public static String bitsToMessage(int[] bits) {
         StringBuilder message = new StringBuilder();
-    
+
         // Process the bits in chunks of 8
-        for (int i = 0; i < bits.length; i += 8) 
-        {
+        for (int i = 0; i < bits.length; i += 8) {
             // Ensure we have 8 bits to form a byte
-            if (i + 7 < bits.length) 
-            {
+            if (i + 7 < bits.length) {
                 int asciiValue = 0;
-    
+
                 // Convert the 8 bits to a single ASCII value
-                for (int j = 0; j < 8; j++) 
-                {
+                for (int j = 0; j < 8; j++) {
                     asciiValue = (asciiValue << 1) | bits[i + j]; // Shift left and add the current bit
                 }
-    
+
                 // Convert ASCII value to character and append to message
-                message.append((char)asciiValue);
+                message.append((char) asciiValue);
             }
         }
-        
-        //Return converted message
+
+        // Return converted message
         return message.toString();
     }
 }
